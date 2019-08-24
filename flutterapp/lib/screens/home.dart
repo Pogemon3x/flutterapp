ea4168bca4e2b67273982875e5316ade7faeafee
pogemon <pogemon@gmail.com>
2019-08-24 16:38:26 +0700

    First day OK

--- flutterapp/lib/screens/home.dart
+++ flutterapp/lib/screens/home.dart
@@ -10,6 +10,39 @@ class _HomeState extends State<Home> {
 
   // Method
 
+  Widget mySizebox(){
+    return SizedBox(width: 5.0,height: 10.0,);
+  }
+
+  Widget signInButton() {
+    return Expanded(
+      child: RaisedButton(color:Colors.blue[400],
+        child: Text('Sign In',style:TextStyle(color: Colors.white)),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signUpButton() {
+    return Expanded(
+        child: OutlineButton(
+      child: Text('Sign Up',style:TextStyle(color:Colors.blue[400])),
+      onPressed: () {},
+    ));
+  }
+
+  Widget showButton() {
+    return Container(
+        margin: EdgeInsets.only(left: 50.0, right: 50.0),
+        child: Row(
+          children: <Widget>[
+            signInButton(),
+            mySizebox(),
+            signUpButton(),
+          ],
+        ));
+  }
+
   Widget showLogo() {
     return Container(
         alignment: Alignment.center,
@@ -18,18 +51,27 @@ class _HomeState extends State<Home> {
   }
 
   Widget showAppName() {
-    return Text('This is my Home');
+    return Text('This is my Home',
+        style: TextStyle(
+          fontSize: 35.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.blue[300],
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          showLogo(),
-          showAppName(),
-        ],
+      body: Container(decoration: BoxDecoration(gradient: RadialGradient(colors: [Colors.white,Colors.blue],radius: 1.0,)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            showLogo(),
+            mySizebox(),
+            showAppName(),
+            showButton(),
+          ],
+        ),
       ),
     );
   }
